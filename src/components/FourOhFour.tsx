17252c88dcc402de732c1aa25188ec53b132289f
shadowqcom <42204012+shadowqcom@users.noreply.github.com>
2025-08-26 20:58:06 +0800

    Update FourOhFour.tsx

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -8,7 +8,7 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          Oops, that&apos;s a <span className="underline decoration-red-500 decoration-wavy">four-oh-four</span>.
+          Oops, that&apos;s a <span className="underline decoration-red-500 decoration-wavy">404</span>.
         </div>
         <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
           {errorMsg}
