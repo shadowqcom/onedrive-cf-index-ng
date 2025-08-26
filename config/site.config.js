3938527fcf06dfee3bcd7cebf564f3d41c8624f0
shadowqcom <42204012+shadowqcom@users.noreply.github.com>
2025-08-26 20:47:08 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🐔 nothing', '/🐔 nothing/std盛天', '/🐔 nothing/dr定制打包', '/🐔 nothing/zt费控'],
+  protectedRoutes: ['/🐔 nothing/std盛天', '/🐔 nothing/dr定制打包', '/🐔 nothing/zt费控'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
